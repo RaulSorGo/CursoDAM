d0735bc53f7a6017bfd3ca89705be4ad63707e21
RaulSorGo <raulsorgo@gmail.com>
2021-10-27 10:18:19 +0200

    j

---
+++ diccionarios.py
@@ -0,0 +1,11 @@
+#repaso funciones
+
+#funcio(a,b,c,d=1, *args, **kywargs)
+
+diccionario = {
+    'nombre': 'fernando',
+    'apellido1':'lopez',
+    'notas': [1,2,3,4,5]
+
+}
+print(type(diccionario))
