5fe50e669f7ed03f4bea0563d4dd45c0a8b2ad32
RaulSorGo <raulsorgo@gmail.com>
2021-10-27 10:26:41 +0200

    actu

--- diccionarios.py
+++ diccionarios.py
@@ -8,4 +8,10 @@ diccionario = {
     'notas': [1,2,3,4,5]
 
 }
+diccionario['apellido2']= 'garcia'
+diccionario['apellido1']= 'lópez'
 print(type(diccionario))
+print(diccionario['nombre'])
+print(diccionario['apellido1'])
+print(diccionario['apellido2'])
+print(f"Nombre completo: {diccionario['nombre']} {diccionario['apellido1']}")
